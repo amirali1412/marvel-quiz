5799faa77abd99620b82bf71d03c8ee961d9bb66
amirali1412 <amirali14@hotmail.co.uk>
2021-07-15 12:07:39 +0000

    Add array of objects in jS

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+/** Array of Objects */
+
+const quiz = [
+    {
+        q:"What is the Incredible Hulk's real name?",
+        options:["Bruce Willis","Bruce Banner","Tony Stark","Steve Rodgers"],
+        answer:1
+    },
+    {
+        q:"Which city is Spiderman from?",
+        options:["Los Angeles","London","Chicago","New York City"],
+        answer:3
+    },
+    {
+        q:"What weapon does Thor use?",
+        options:["Hammer","Sword","Axe","Shield"],
+        answer:0
+    },
+    {
+        q:"Which War did Captain America fight in?",
+        options:["World War 1","World War 2","Vietnam War","Gulf War"],
+        answer:1
+    },
+    {
+        q:"What material is Captain America's shield made of?",
+        options:["Steel","Iron","Vibranium","Platinum"],
+        answer:2
+    },
+    {
+        q:"What land is the Black Panther from?",
+        options:["Wakanda","Kenya","Egypt","Cameroon"],
+        answer:0
+    },
+    {
+        q:"Which character can shrink so tiny that they cannot be seen by the naked eye?",
+        options:["Incredible Hulk","Hawkeye","Falcon","Antman"],
+        answer:3
+    },
+    {
+        q:"Which Realm does Thor come from?",
+        options:["Midgard","Asgard","Jotunheim","Svartalfheim"],
+        answer:1
+    },
+    {
+        q:"Which Character's weapon is a bow and arrow?",
+        options:["Iron Man","Thor","Black Widow","Hawkeye"],
+        answer:3
+    },
+    {
+        q:"Which evil character wore a glove which held all the infinity stones?",
+        options:["Thanos","Dr Strange","Loki","Winter Soldier"],
+        answer:0
+    },
+    {
+        q:"What group of characters is professor Charles Xavier the leader of?",
+        options:["Thunderbolts","Fantastic Four","X-men","Avengers"],
+        answer:2
+    },
+    {
+        q:"What is Iron Man's real name?",
+        options:["Thor Odinson","Nick Fury","Tony Stark","Steve Rodgers"],
+        answer:2
+    },
+    {
+        q:"Which character wears an eye patch?",
+        options:["Nick Fury","Bruce Banner","Peter Parker","Jean Grey"],
+        answer:0
+    },
+    {
+        q:"What color skin does Mystique have in her natural form?",
+        options:["Red","Yellow","Orange","Blue"],
+        answer:3
+    },
+    {
+        q:"What type of creatures does the character Blade hunt?",
+        options:["Werewolves","Vampires","Elves","Dwarves"],
+        answer:1
+    },
+    {
+        q:"How many infinity stones are there?",
+        options:["3","5","6","8"],
+        answer:2
+    },
+    {
+        q:"Which of these characters can heal themselves?",
+        options:["Wolverine","Black Widow","Cyclops","Nightcrawler"],
+        answer:0
+    },
+    {
+        q:"What country is Colussus from?",
+        options:["Sweden","Brazil","Turkey","Russia"],
+        answer:3
+    },
+    {
+        q:"What character does Jean Grey evolve into?",
+        options:["Blue Robin","Dark Phoenix","Purple Dragon","Black Widow"],
+        answer:2
+    },
+    {
+        q:"How many weapons does the Human Torch carry on him?",
+        options:["1","2","0","3"],
+        answer:1
+    },
+]
